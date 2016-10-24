d4f375ca76dc40dcb3a1741444870b5a1f16efdf
vipacer <vipacer@163.com>
2016-10-24 20:33:58 +0800

    define SCPP_THROW_EXCEPTION_ON_BUG

--- SafeCPlusPlus/main.cpp
+++ SafeCPlusPlus/main.cpp
@@ -3,15 +3,27 @@
 
 using std::cout;
 using std::endl;
+using std::exception;
+using std::cerr;
+
 
 int main(int agrc, char ** argv)
 {
 	cout << "Hello,SCPP_Assert" << endl;
-	double stock_price = 100.0;
-	SCPP_ASSERT(stock_price > 0. && stock_price <= 1.e6,
-		"Stock price " << stock_price << " is out range");
-	stock_price = -1.0;
-	SCPP_ASSERT(stock_price > 0. && stock_price <= 1.e6,
-		"Stock price " << stock_price << " is out range");
+	try
+	{
+		double stock_price = 100.0;
+		SCPP_ASSERT(stock_price > 0. && stock_price <= 1.e6,
+			"Stock price " << stock_price << " is out range");
+		stock_price = -1.0;
+		SCPP_ASSERT(stock_price > 0. && stock_price <= 1.e6,
+			"Stock price " << stock_price << " is out range");
+	}
+	catch (const exception &ex)
+	{
+		cerr << "Exception caught in " << __FILE__ << " #" << __LINE__ << ":\n"
+			<< ex.what() << endl;
+	}
+	
 	return 0;
 }
